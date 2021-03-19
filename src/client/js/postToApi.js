1638127f007566a201c595675a48b14fbc6de423
abdlla-nasser <bedo2boy@gmail.com>
2021-03-20 01:59:23 +0200

    fixed 404 on running npm run build-dev and npm start

--- src/client/js/postToApi.js
+++ src/client/js/postToApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 export default async function (url) {
   console.log("here")
-  const data = await axios.post("/api", { url }).then(res => res.data)
+  const data = await axios
+    .post(`http://localhost:8081/api`, { url })
+    .then((res) => res.data);
   return data;
 }
--- src/server/start.js
+++ src/server/start.js
@@ -1,5 +1,5 @@
-const app = require("./index")
-const PORT = process.env.PORT || 5000;
+const app = require("./index");
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
